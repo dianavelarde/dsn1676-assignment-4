360ea305ac7c21d826d5eab61593e5b916461c12
dianavelarde <van_di09@hotmail.com>
2015-02-12 16:27:04 -0500

    added fruits to html

--- js/main.js
+++ js/main.js
@@ -1,23 +1,51 @@
 var blueberries = {
-	info: "Vaccinium corymbosum",
+	info: 'Vaccinium corymbosum',
 	growsOnShrub: true,
 	energy: 240,
 	carbohydrates: 14.49,
 	protein: 0.74
 };
+console.log(blueberries.info);
 
 var grapes = {
-	info: "Vitis vinifera",
+	info: 'Vitis vinifera',
 	growsOnShrub: false,
 	energy: 288,
 	carbohydrates: 18.1,
 	protein: 0.72
 };
+console.log(grapes.info);
 
 var redCurrant = {
-	info: "Ribes rubrum",
+	info: 'Ribes rubrum',
 	growsOnShrub: true,
 	energy: 234,
 	carbohydrates: 13.8,
 	protein: 1.4
 };
+console.log(redCurrant.info);
+
+var writeFruitInfo = function (fruit) {
+    document.write ('<h1>' + fruit.info  + '</h1>');
+    
+    document.write('<dl>');
+    
+	document.write('<dt>Grows on shrub</dt>');
+	document.write('<dd>' + fruit.growsOnShrub +'</dd>');
+    
+	document.write('<dt>Energy</dt>');
+	document.write('<dd>'+ fruit.energy + '</dd>');
+    
+	document.write('<dt>Carbohydrates</dt>');
+	document.write('<dd>' + fruit.carbohydrates +'</dd>');
+    
+	document.write('<dt>Protein</dt>');
+	document.write('<dd> ' + fruit.protein + '</dd>');
+    
+	document.write('</dl>');
+};
+    
+
+writeFruitInfo(blueberries);
+writeFruitInfo(grapes);
+writeFruitInfo(redCurrant);
