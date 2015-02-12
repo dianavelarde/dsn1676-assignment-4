6c6f845cf09f4a89e805be292ece145bc70a282d
dianavelarde <van_di09@hotmail.com>
2015-02-12 15:51:27 -0500

    js fruit elements added

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var blueberries = {
+	info: "Vaccinium corymbosum",
+	growsOnShrub: true,
+	energy: 240,
+	carbohydrates: 14.49,
+	protein: 0.74
+};
+
+var grapes = {
+	info: "Vitis vinifera",
+	growsOnShrub: false,
+	energy: 288,
+	carbohydrates: 18.1,
+	protein: 0.72
+};
+
+var redCurrant = {
+	info: "Ribes rubrum",
+	growsOnShrub: true,
+	energy: 234,
+	carbohydrates: 13.8,
+	protein: 1.4
+};
